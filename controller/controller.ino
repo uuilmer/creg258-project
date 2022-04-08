f51cc38142a56117618edf2eefd79dcde4ad351a
Wilmer <wilmerh701@gmail.com>
2022-04-08 01:45:08 -0400

    Added fixes and comments

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,40 +1,116 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Wire.h>
+#include <SparkFunLSM9DS1.h>
 
-RF24 radio(6, 7);                // CE, CSN
-const byte address[6] = "00001"; // Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
-uint8_t SentMessage[1] = {0};
+/* Constants for use with transmitter */
+// Pins that we connected to CE and CSN of the transmitter
+RF24 radio(6, 7);
+// Address that must be matching with receivere for a connection
+const byte address[6] = "00001";
+// Buffer containing messages we wish to send
+String GyroscopeReading[1] = {""};
+
+// LED that we will use to indicate proper connection to the transmitter
 #define LED 2
 
+/* Constants for IMU readings */
+// The imu object that will provide us with gyroscope readings
+LSM9DS1 imu;
+// Speed at which wee will send gyroscope updates
+#define UPDATE_SPEED 50
+// Keep track of print time
+static unsigned long lastPrint = 0;
+
+/**
+ * We must set up the transmitter, and the IMU
+ */
 void setup()
 {
+    // Setup LED indicator
     pinMode(LED, OUTPUT);
+
+    // Setup required modules
+    setupIMUHelper();
+    setupRadioHelper();
+}
+
+/**
+ * Attempt to establish a radio connection
+ */
+void setupRadioHelper()
+{
     if (radio.begin(6, 7))
-    { // Starting the Wireless communication
-        Serial.println("success");
-    }
-    else
     {
-        Serial.println("failed");
+        Serial.println("Connected to transmitter...");
+        // Indicate a physical successful transmitter connection
         digitalWrite(LED, HIGH);
     }
-    radio.openWritingPipe(address); // Setting the address where we will send the data
-    radio.setPALevel(RF24_PA_MIN);  // You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
-    radio.stopListening();          // This sets the module as transmitter
-}
-void loop()
-{
-    SentMessage[0]++;
-    Serial.println(SentMessage[0]);
-    digitalWrite(LED, SentMessage[0] % 2 == 0 ? HIGH : LOW);
-    if (radio.write(SentMessage, sizeof(SentMessage)))
-    { // Sending the message to receiver
-        digitalWrite(LED, SentMessage[0] % 2 == 0 ? HIGH : LOW);
-    }
     else
     {
-        SentMessage[0]--;
+        Serial.println("Failed to connect to transmitter...");
+        return;
     }
-    delay(1000);
+
+    // Setup the transmitter
+    radio.openWritingPipe(address);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.stopListening();
+}
+
+/**
+ * Attempt to setup IMU for gyroscope readings
+ */
+void setupIMUHelper()
+{
+    Wire.begin();
+
+    if (imu.begin() == false)
+    {
+        Serial.println("Failed to communicate with LSM9DS1.");
+        Serial.println("Double-check wiring.");
+        while (1)
+            ;
+    }
+}
+
+/**
+ * At each iteration, we must read the gyroscope and transmit through transmitter
+ */
+void loop()
+{
+    // Wait until we can measure again
+    if ((lastPrint + UPDATE_SPEED) >= millis())
+    {
+        return;
+    }
+
+    GyroscopeReading[0] = readGyroscope();
+
+    // Send reading to receiver
+    for (auto c : GyroscopeReading[0])
+    {
+        radio.write(&c, sizeof(c));
+    }
+
+    // Update lastPrint time
+    lastPrint = millis();
+}
+
+/**
+ * Attempt to read a gyroscope reading from IMU
+ */
+String readGyroscope()
+{
+    // Update the sensor values whenever new data is available
+    if (imu.gyroAvailable())
+    {
+        // To read from the gyroscope,  first call the
+        // readGyro() function. When it exits, it'll update the
+        // gx, gy, and gz variables with the most current data.
+        imu.readGyro();
+    }
+
+    return String(imu.calcGyro(imu.gx)) + " " + String(imu.calcGyro(imu.gy)) + " " + String(imu.calcGyro(imu.gz)) + ",";
 }
--- pc_receiver/pc_receiver.ino
+++ pc_receiver/pc_receiver.ino
@@ -1,28 +1,88 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <SparkFunLSM9DS1.h>
+#include "Adafruit_TinyUSB.h"
 
-RF24 radio(6, 7); // CE, CSN
+/* Constants for use with receiver */
+// Pins that we connected to CE and CSN of the receiver
+RF24 radio(6, 7);
+// Address that must be matching with receivere for a connection
 const byte address[6] = "00001";
-uint8_t ReceivedMessage[1] = {0};
+// Buffer containing messages we wish to send
+char ReceivedGyroscopeReading[1] = {'0'};
+
+// LED that we will use to indicate proper connection to the receiver
 #define LED 2
 
+/* Constants for TinyUSB */
+// USB WebUSB object
+Adafruit_USBD_WebUSB usb_web;
+// Landing Page: scheme (0: http, 1: https), url
+WEBUSB_URL_DEF(landingPage, 1 /*http*/, "compe-capstone.herokuapp.com/");
+
+/**
+ * We must set up the transmitter, tinyUSB, and the IMU
+ */
 void setup()
 {
+    // Setup LED indicator
     pinMode(LED, OUTPUT);
-    Serial.begin(9600);
-    radio.begin(6, 7);
-    radio.openReadingPipe(0, address); // Setting the address at which we will receive the data
-    radio.setPALevel(RF24_PA_MIN);     // You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
-    radio.startListening();            // This sets the module as receiver
+
+    // Setup required modules
+    setupUSBHelper();
+    setupRadioHelper();
 }
+
+/**
+ * Attempt to establish a radio connection
+ */
+void setupRadioHelper()
+{
+    if (radio.begin(6, 7))
+    {
+        Serial.println("Connected to receiver...");
+        // Indicate a physical successful receiver connection
+        digitalWrite(LED, HIGH);
+    }
+    else
+    {
+        Serial.println("Failed to connect to receiver...");
+        return;
+    }
+
+    // Setup the receiver
+    radio.openReadingPipe(0, address);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.startListening();
+}
+
+/**
+ * Attempt to setup USB transmission
+ */
+void setupUSBHelper()
+{
+#if defined(ARDUINO_ARCH_MBED) && defined(ARDUINO_ARCH_RP2040)
+    // Manual begin() is required on core without built-in support for TinyUSB such as mbed rp2040
+    TinyUSB_Device_Init(0);
+#endif
+
+    usb_web.setLandingPage(&landingPage);
+    usb_web.begin();
+
+    // Wait until device mounted
+    while (!USBDevice.mounted())
+        delay(1);
+}
+
+/**
+ * At each iteration, we must get from transmitter and send to USB
+ */
 void loop()
 {
     if (radio.available())
     {
-        radio.read(ReceivedMessage, sizeof(ReceivedMessage)); // Reading the data
-        Serial.println(ReceivedMessage[0]);
-        digitalWrite(LED, ReceivedMessage[0] % 2 == 0 ? HIGH : LOW);
+        radio.read(ReceivedGyroscopeReading, sizeof(ReceivedGyroscopeReading));
+        usb_web.write(ReceivedGyroscopeReading[0]);
     }
-    delay(10);
 }
